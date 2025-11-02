aea011712844f454476246c872df68f724e9187f
kubota6646 <240894337+kubota6646@users.noreply.github.com>
2025-11-03 07:14:02 +0900

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set DIR=%~dp0
-if "%DIR%"=="" set DIR=.
-set DIR=%DIR:~0,-1%
-set GRADLE_HOME=%DIR%\gradle\wrapper\..\..
-set JAVA_HOME=%JAVA_HOME%
-set PATH=%JAVA_HOME%\bin;%GRADLE_HOME%\bin;%PATH%
-jav...
